7ae3046a9eb870ac0b786dc4df92476271b8fa09
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2025-04-26 21:04:50 +0900

    web server

--- main.tf
+++ main.tf
@@ -5,4 +5,28 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-05206bf8aecfc7ae6"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  # 0.0.0.0/0からポート8080に対する内向きのTCPリクエストを許可
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
