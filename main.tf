94cf6334602aef117ec1aa778129be2e7a9eaa0c
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2025-04-27 11:56:23 +0900

    auto scaling ec2

--- main.tf
+++ main.tf
@@ -2,20 +2,46 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-05206bf8aecfc7ae6"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# EC2インスタンスの起動設定
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-05206bf8aecfc7ae6"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
-  user_data_replace_on_change = true
 
-  tags = {
-    Name = "terraform-example"
+  # EC2インスタンスの起動設定が変更された場合、ASGを再作成する
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
